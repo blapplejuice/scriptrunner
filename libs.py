4b6b88af1863a48a13890780afc21ed290605f3a
blapplejuice <developer egg>
2020-02-25 21:27:46 +0000

    developing radiobuttons and features

--- libs.py
+++ libs.py
@@ -4,17 +4,55 @@ def tk():
 
     root = Tk();
     frame = Frame(root);
+    v = IntVar();
+    v.set(1);
+    os= [
+        ("Windows", 1),
+        ("Linux", 2)
+    ]
     
     def close():
         root.destroy();
     
+    def process():
+        script = pad.get();
+
+
     frame.pack();
     root.title("scriptrunner");
 
-    button = Button(frame,
-                    width=20,
+    exitbtn = Button(frame,
+                    width=50,
                     text="Exit",
                     command=close);
-    button.pack(side=TOP);
+    exitbtn.pack(side=TOP);
+    
+    pad = Text(frame,
+                width=50);
+    pad.pack();
+
+    windowsradio = Radiobutton(frame,
+                            text="Windows",
+                            variable=v,
+                            value=1);
+    windowsradio.pack(side=BOTTOM);
+
+    linuxradio = Radiobutton(frame,
+                            text="Linux",
+                            variable=v,
+                            value=2);
+    linuxradio.pack(side=BOTTOM);
+
+    osinfo = Label(frame,
+                    text="Operating System",
+                    justify=LEFT)
+    osinfo.pack(side=BOTTOM)
+
+    processbtn = Button(frame,
+                        width=50,
+                        text="Process",
+                        command=process);
+    processbtn.pack(side=BOTTOM)
+
 
     root.mainloop();
